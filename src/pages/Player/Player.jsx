65a273a8f5822c9a6ecd266d82caace9b649e6e3
K-Nithish-Kumar <nithish81812@gmail.com>
2025-12-13 21:41:02 +0530

    Adding routing to thumbnail to play the trailer video

--- src/pages/Player/Player.jsx
+++ src/pages/Player/Player.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Player.css'
+import back_arrow_icon from '../../assets/back_arrow_icon.png'
+import { useNavigate, useParams } from 'react-router-dom'
 
 const Player = () => {
+
+  const {id} = useParams();
+  const navigate = useNavigate();
+  
+  const[apiData,setApiData] = useState({
+    name:"",
+    key: "",
+    published_at:"",
+    type:""
+  });
+
+  const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5YjA2YzE5NTdmOGNlYmI2NjUzMDgzOGI3NjU4NmZjZCIsIm5iZiI6MTc2NTYzODcyNi44MzksInN1YiI6IjY5M2Q4MjQ2ZTk4YjQ1MDY1MDk5ZTlkNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.76TWSr_uYfIKYx-Dwn_0R3M6MNGaW3Sct9AE1BGLXAM'
+  }
+};
+
+useEffect(()=> {
+  fetch(`https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`, options)
+  .then(res => res.json())
+  .then(res => setApiData(res.results[0]))
+  .catch(err => console.error(err));
+},[])
+
+
+
+
   return (
-    <div className='player'></div>
+    <div className='player'>
+      <img src={back_arrow_icon} alt="" onClick={()=>{navigate('/')}}/>
+      <iframe width="90%" height="90%" src={`https://www.youtube.com/embed/${apiData.key}`} title="trailer" frameBorder="0" allowFullScreen></iframe>
+      <div className="player-info">
+        <p>{apiData.published_at.slice(0,10)}</p>
+        <p>{apiData.name}</p>
+        <p>{apiData.type}</p>
+      </div>
+    </div>
   )
 }
 
